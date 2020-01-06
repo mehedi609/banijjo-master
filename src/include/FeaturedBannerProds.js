2bb7adba12e296072bfbe9923c78bb4dafd26a28
Rokibuzzaman <sourovh207@gmail.com>
2020-01-06 15:44:54 +0600

    mobile view file edit

--- src/include/FeaturedBannerProds.js
+++ src/include/FeaturedBannerProds.js
@@ -15,18 +15,18 @@ const FeaturedBannerProds = ({ prodsImags }) => {
 
   return (
     <OwlCarousel className="owl-theme" margin={10} {...options}>
-      {prodsImags.map(({ home_image }, index) => (
-        <div className="frameSlider" style={{ borderBottom: "1px solid #ddd" }}>
-          <span className="helperSlider" style={{ paddingBottom: "10px" }}>
-            <img
-              src={`${fileUrl}/upload/product/productImages/${home_image}`}
-              alt="Product Image."
-            />
-          </span>
-        </div>
+    {prodsImags.map(({ home_image }, index) => (
+      <div className="frameSlider">
+      <span className="helperSlider" style={{ paddingBottom: "10px" }}>
+      <img
+      src={`${fileUrl}/upload/product/productImages/${home_image}`}
+      alt="Product Image."
+      />
+      </span>
+      </div>
       ))}
-    </OwlCarousel>
-  );
-};
+      </OwlCarousel>
+      );
+    };
 
-export default FeaturedBannerProds;
+    export default FeaturedBannerProds;
